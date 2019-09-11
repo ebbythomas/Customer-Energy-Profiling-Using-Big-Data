c024a5ca84400a5ce4e139ad64877959b516346b
Ebby Thomas <31952161+ebbythomas@users.noreply.github.com>
2019-09-11 17:30:58 +1000

    Delete formula.jl

--- DataProcessing/formula.jl
+++
@@ -1,4 +0,0 @@
-dfNames = names(newDf)
-responseVar = Meta.parse("KWH")
-predictorVars = dfNames[dfNames .!= responseVar]
-fm = Formula(responseVar, Expr(:call, :+, predictorVars...)) #works but has warning
